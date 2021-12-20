ed268a0195d2b4a7d534e474faaa67606f2a11a2
Ankurneural <ankurparmar42@gmail.com>
2021-12-19 23:16:36 -0800

    Yolo version 3 Video Object detection

--- model.py
+++ model.py
@@ -5,9 +5,12 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, 
 
 # Images
 img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+# video1 = r'C:\Apps\Masters_SJSU\Prof_Stas_ISA\Ball Boy\video_ball_track\data\video\Pexels Videos 992693.mp4'
+
+
 
 # Inference
 results = model(img)
-
+# res = model(video1)
 # Results
 results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
